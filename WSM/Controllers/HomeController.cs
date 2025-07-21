2916282cf89c4ff0ca62b09faf142f7ebb604296
xiaer6289 <164221150+xiaer6289@users.noreply.github.com>
2025-07-21 16:54:10 +0800

    Adjust for common header search bar placeholder added restock file changes on app.css

--- WSM/Controllers/HomeController.cs
+++ WSM/Controllers/HomeController.cs
@@ -20,7 +20,9 @@ namespace WSM.Controllers
         // GET: /Home/Index
         public IActionResult Index()
         {
-
+            // For Layout.cshtml search bar
+            ViewBag.SearchContext = "Restock";
+            ViewBag.SearchPlaceholder = "Search";
             return View();
         }
 
@@ -49,14 +51,14 @@ namespace WSM.Controllers
 
             Func<OrderDetail, object> fn = sort switch
             {
-                "Id" => s => s.Id,
-                "Name" => s => s.Foods,
-                "Seat" => s => s.SeatNo,
-                "Quantity" => s => s.Quantity,
-                "status" => s => s.Status,
+                "Id"    => s => s.Id,
+                //"Name" => s => s.Foods,
+                //"Seat" => s => s.SeatNo,
+                //"Quantity" => s => s.Quantity,
+                //"status" => s => s.Status,
                 "Total" => s => s.TotalPrice,
-                "Date" => s => s.OrderDate,
-                _ => s => s.Id
+                "Date"  => s => s.OrderDate,
+                _       => s => s.Id
             };
 
             var sorted = dir == "des" ?
@@ -89,7 +91,73 @@ namespace WSM.Controllers
 
         }
 
+        public IActionResult Detail(string? id)
+        {
+            var m = db.OrderDetails.Find(id);
+
+            if (m == null)
+            {
+                return RedirectToAction("OrderHistory");
+            }
+
+            return View(m);
+        }
+
+        public IActionResult Restock(string? id, string? sort, string? dir, int page = 1)
+        {
+            // For Layout.cshtml search bar
+            ViewBag.SearchContext = "Restock";
+            ViewBag.SearchPlaceholder = "Search by Ingredient ID/Name";
+
+            // Searching
+            ViewBag.Name = id = id?.Trim() ?? "";
+            var searched = db.Ingredients
+                             .Where(s => s.Id.Contains(id));
+
+            //sorting
+            ViewBag.Sort = sort;
+            ViewBag.Dir = dir;
+
+            Func<Ingredient, object> fn = sort switch
+            {
+                "Id"         => s => s.Id,
+                "Name"       => s => s.Name,
+                "Quantity"   => s => s.Quantity,
+                "Weight(kg)" => s => s.Kilogram,
+                "Price"      => s => s.Price,
+                "Total"      => s => s.TotalPrice,
+                _            => s => s.Id
+            };
+
+            var sorted = dir == "des" ?
+                     searched.OrderByDescending(fn) :
+                     searched.OrderBy(fn);
 
 
-    }   
-}
+            // (3) Paging ---------------------------
+            if (page < 1)
+            {
+                return RedirectToAction(null, new { id, sort, dir, page = 1 });
+            }
+
+
+            var m = sorted.ToPagedList(page, 10);
+
+
+            if (page > m.PageCount && m.PageCount > 0)
+            {
+                return RedirectToAction(null, new { id, sort, dir, page = m.PageCount });
+            }
+
+            if (Request.Headers["X-Requested-With"] == "XMLHttpRequest")
+            {
+                return PartialView("Restock", m);
+            }
+
+
+            return View(m);
+
+        }
+    }
+}   
+
