e21c28d25d67b1a4ad35fca2bb1f1388327cc371
limhongying <limhongying15@gmail.com>
2025-07-28 01:09:12 +0800

    Add Payment feature (incomplete) and reslove conflict in HomeController.cs

---
+++ WSM/Models/StripeSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace WMS.Models
+{
+    public class StripeSettings
+    {
+        public string SecretKey { get; set; }
+        public string PublicKey { get; set; }
+    }
+}
--- WSM/Program.cs
+++ WSM/Program.cs
@@ -1,10 +1,14 @@
 using Microsoft.EntityFrameworkCore;
+using WMS.Models;
 using WSM.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+//Stripe 
+builder.Services.Configure<StripeSettings>(builder.Configuration.GetSection("StripeSettings"));
+
 
 builder.Services.AddSqlServer<DB>($@"
     Data Source=(LocalDB)\MSSQLLocalDB;
