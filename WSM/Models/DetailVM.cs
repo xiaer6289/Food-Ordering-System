da1d69e899286e02261c1edaa8f9e9287b3725ae
xiaer6289 <164221150+xiaer6289@users.noreply.github.com>
2025-09-19 21:20:57 +0800

    remove unecessary codes

--- WSM/Models/DetailVM.cs
+++ WSM/Models/DetailVM.cs
@@ -5,6 +5,4 @@ public class DetailVM      //wrap multiple table
 {
     public OrderDetail orderDetail { get; set; }
     public Company company { get; set; }
-    public Staff staff { get; set; }
-    public Admin admin { get; set; }
 }
