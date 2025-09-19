04adcfe0e2c2731ca40dd45b801ef4dc7f855fa6
YC <cherng.yw@gmail.com>
2025-09-19 18:23:38 +0800

    fooddetail photo

--- WSM/Controllers/MenuController.cs
+++ WSM/Controllers/MenuController.cs
@@ -16,33 +16,25 @@ namespace WMS.Controllers
         }
 
         // GET: /Menu/FoodListing
-        public IActionResult FoodListing(string search = null, string categoryId = "All", string SeatNo = null)
+        public IActionResult FoodListing(string? search = null, string categoryId = "All", string SeatNo = null)
         {
-            // Setup for search bar
             ViewBag.SearchContext = "Food";
-            ViewBag.SearchPlaceholder = "Search by Food Name or ID";
-            ViewBag.SearchTerm = search?.Trim() ?? "";
+            ViewBag.SearchPlaceholder = "Search by Food Name";
+
+            search = search?.Trim() ?? "";
+            ViewBag.SearchTerm = search;
             ViewBag.SeatNo = SeatNo;
 
             IQueryable<Food> foods = _db.Foods;
 
-            // Filter by category
             if (!string.IsNullOrEmpty(categoryId) && categoryId != "All")
             {
                 foods = foods.Where(f => f.CategoryId == categoryId);
             }
 
-            // Search by ID or Name
             if (!string.IsNullOrEmpty(search))
             {
-                if (int.TryParse(search, out int foodId) && foods.Any(f => f.Id == foodId))
-                {
-                    foods = foods.Where(f => f.Id == foodId);
-                }
-                else
-                {
-                    foods = foods.Where(f => f.Name.Contains(search));
-                }
+                foods = foods.Where(f => f.Name.Contains(search));
             }
 
             ViewBag.Categories = _db.Categories.ToList();
