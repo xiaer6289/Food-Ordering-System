f035e852f1b63be55c69ad0f100ac8388f6c2976
YC <cherng.yw@gmail.com>
2025-09-19 12:47:30 +0800

    Category left delete validation

--- WSM/Models/ViewCategory.cs
+++ WSM/Models/ViewCategory.cs
@@ -7,6 +7,8 @@ public class CategoryVM
     [StringLength(3)]
     public string Id { get; set; }
 
-    [StringLength(100, ErrorMessage = "Category name cannot exceed 100 characters.")]
+    [StringLength(30, ErrorMessage = "Category name cannot exceed 30 characters.")]
+    [Required(ErrorMessage = "Category name is required")]
+    [RegularExpression(@"^[A-Za-z\s]+$", ErrorMessage = "Category name can only contain letters and spaces")]
     public string Name { get; set; }
 }
